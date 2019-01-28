639b053c67534cf03b954f2b0f99115f8266a73f
Samuele Reghenzi <sam@reghenzi.cloud>
2019-01-28 15:15:47 +0100

    fix methods

--- src/main/g8/src/main/scala/$package$/utils/AutoValidate.scala
+++ src/main/g8/src/main/scala/$package$/utils/AutoValidate.scala
@@ -4,5 +4,6 @@ import java.util
 import scala.collection.JavaConverters._
 
 class AutoValidate extends Authentication {
-  override def validateToken(token: String): util.Map[String, AnyRef] = Map[String, AnyRef]("token" -> token, "autoValidation" -> "true").asJava
+  override  def validateToken(token:String):Map[String,AnyRef] = Map("token" -> token, "autoValidation" -> "true")
+
 }
