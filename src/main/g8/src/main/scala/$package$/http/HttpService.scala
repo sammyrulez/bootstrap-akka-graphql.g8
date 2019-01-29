919a83f97bb38235aaaff818ff5ae8ed8adc8024
Samuele Reghenzi <sam@reghenzi.cloud>
2019-01-29 09:05:46 +0100

    moved to config

--- src/main/g8/src/main/scala/$package$/http/HttpService.scala
+++ src/main/g8/src/main/scala/$package$/http/HttpService.scala
@@ -12,7 +12,6 @@ import scala.concurrent.ExecutionContext
 
 class HttpService(graphQLContext: GraphQLContextServices)(implicit executionContext: ExecutionContext, actorSystem: ActorSystem, authService: AuthService) extends Configuration {
 
-  val settings = CorsSettings.defaultSettings.copy(allowedMethods = List(GET, POST, PUT, HEAD, OPTIONS, DELETE))
   val graphqlRoute = new GraphqlRoute(graphQLContext)
   val routes =
     cors(settings) {
